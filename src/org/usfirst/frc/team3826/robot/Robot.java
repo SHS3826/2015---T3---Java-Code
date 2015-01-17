851f1420051f1b9fb70b4306acccd7a4f0b370b5
Skynet Robotics NW - FRC Team 3826 <joshrmyers@gmail.com>
2015-01-17 11:31:38 -0800

    Basic Drive Code
    Basic Mecanum Drive Code, Correct Orientation for T2.

---
+++ src/org/usfirst/frc/team3826/robot/Robot.java
@@ -0,0 +1,87 @@
+package org.usfirst.frc.team3826.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	RobotDrive robotDrive;
+	Joystick controlStick;
+	int autoLoopCounter;
+    final int frontLeftChannel	= 1;
+    final int rearLeftChannel	= 0;
+    final int frontRightChannel	= 2;
+    final int rearRightChannel	= 3;
+    final int joystickChannel	= 0;
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	controlStick = new Joystick(0);
+        robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
+        robotDrive.setSafetyEnabled(true);
+    	robotDrive.setExpiration(10);
+    	robotDrive.setInvertedMotor(MotorType.kFrontRight, true);	// invert the left side motors
+    	robotDrive.setInvertedMotor(MotorType.kRearRight, true);	// you may need to change or remove this to match your robot
+    }
+    
+    /**
+     * This function is run once each time the robot enters autonomous mode
+     */
+    public void autonomousInit() {
+    	///autoLoopCounter = 0;
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	//if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		//{
+		//	myRobot.drive(-0.5, 0.0); 	// drive forwards half speed
+		//	autoLoopCounter++;
+		//	} else {
+		//	myRobot.drive(0.0, 0.0); 	// stop robot
+		//}
+    }
+    
+    /**
+     * This function is called once each time the robot enters tele-operated mode
+     */
+    public void teleopInit(){
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        //robotDrive.setSafetyEnabled(true);
+        while (isOperatorControl() && isEnabled()) {
+        	
+        	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
+            robotDrive.mecanumDrive_Cartesian(controlStick.getX(), controlStick.getY(), controlStick.getThrottle(), 0);           
+            Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
+        }
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	LiveWindow.run();
+    }
+    
+}
